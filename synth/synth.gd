58caf79fa750b44481b9031c70a638aeb46a0fb6
VargaDot <vancouver_cliparf@simplelogin.com>
2024-11-24 13:51:44 +0100

    Added hair length and color

--- synth/synth.gd
+++ synth/synth.gd
@@ -11,6 +11,8 @@ var age:int
 var build:int
 var height:int
 var eyes:int
+var has_long_hair:bool
+var hair_color:int
 var glasses:bool
 var facial_hair:bool
 var shoe_size:int
--- synthesizer/synthesizer.gd
+++ synthesizer/synthesizer.gd
@@ -9,10 +9,12 @@ func _generate() -> void:
 	synth.is_male = choose_yes_or_no()
 	synth.glasses = choose_yes_or_no()
 	if synth.is_male: synth.facial_hair = choose_yes_or_no()
+	synth.has_long_hair = choose_yes_or_no()
 
 	synth.voice_id = randi_range(0,99)
 	synth.eyes = randi_range(0,5)
 	synth.build = randi_range(0,2)
+	synth.hair_color = randi_range(0,8)
 	synth.height = randi_range(150,200) # this technically allocates a bigger portion of RAM
 	synth.shoe_size = randi_range(0,10)
 	synth.blood_type = randi_range(0,7)
@@ -43,6 +45,25 @@ func _interpret_info():
 		5: eye_color = "Amber"
 		_: eye_color = "Undefined"
 	print("Eye color: " + eye_color)
+	
+	if synth.has_long_hair:
+		print ("Long hair")
+	else:
+		print("Short hair")
+	
+	var hair_color:String
+	match synth.hair_color:
+		0: hair_color = "Blonde"
+		1: hair_color = "Dark Blonde"
+		2: hair_color = "Medium Brown"
+		3: hair_color = "Dark Brown"
+		4: hair_color = "Black"
+		5: hair_color = "Reddish Brown"
+		6: hair_color = "Red"
+		7: hair_color = "Gray"
+		8: hair_color = "White"
+		_: hair_color = "Undefined"
+	print("Hair Color: " + hair_color)
 	
 	print("Height: " + str(synth.height))
 	print("Shoe size: " + str(synth.shoe_size))
